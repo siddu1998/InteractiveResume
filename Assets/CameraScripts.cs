16030df6098d6e12984e7b90ca589d7edf6e5284
Sai Siddartha Maram <smaram_be16@thapar.edu>
2021-06-08 18:02:01 +0530

    test commit

---
+++ Assets/CameraScripts.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScripts : MonoBehaviour
+{
+
+    public GameObject ball;
+    Vector3 offset;
+    public float lerpRate; //rate at which the camera will change its potion to follow the ball.
+    public bool gameOver;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    gameOver = false;
+    offset = ball.transform.position - transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!gameOver){
+            Follow();
+            }
+       
+    }
+
+    void Follow(){
+        Vector3 pos = transform.position;
+        Vector3 targetPos = ball.transform.position - offset;
+        pos = Vector3.Lerp(pos,targetPos,lerpRate*Time.deltaTime);
+        transform.position = pos;
+    }
+}
